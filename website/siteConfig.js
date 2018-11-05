0e9fb9b95ffc9c510ee2d452f2f25f80b06d39c3
fengli <fengli@chalco-steering.com>
2018-11-05 19:17:13 +0800

    修该了配置文件

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,7 +23,7 @@ const users = [
 const siteConfig = {
   title: 'Cocina', // 网站标题.
   tagline: 'Cocina ',
-  url: 'https://jigang-duan.github.io', // Your website URL
+  url: 'https://angry-kitchen.github.io', // Your website URL
   baseUrl: '/cocina-website/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
@@ -31,7 +31,7 @@ const siteConfig = {
 
   // Used for publishing and more
   projectName: 'cocina-website',
-  organizationName: 'jigang-duan',
+  organizationName: 'angry-kitchen',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
